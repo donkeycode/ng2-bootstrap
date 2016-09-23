36b86337308ae1a9bfc3fdc9848d72891c47a7f0
Nicola RIZZO <nicola.n.rizzo@gmail.com>
2016-09-23 15:32:00 +0200

    Set moment locale to fr

--- components/datepicker/date-formatter.ts
+++ components/datepicker/date-formatter.ts
@@ -2,6 +2,7 @@ import * as moment from 'moment';
 
 export class DateFormatter {
   public format(date:Date, format:string):string {
+    moment.locale('fr');
     return moment(date.getTime()).format(format);
   }
 }
